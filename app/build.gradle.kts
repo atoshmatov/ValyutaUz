83d75ea360f38e49bf167c35f3179d4b44faa3ec
a.toshmatov <mobdevali1220@gmail.com>
2024-09-21 08:39:39 +0500

    update app version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "uz.toshmatov.currency"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
