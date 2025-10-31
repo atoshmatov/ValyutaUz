e79df4ef0fc52855a2dc935b78bf43fa8807194f
a.toshmatov <mobdevali1220@gmail.coom>
2025-10-31 16:46:56 +0500

    page size 16 kb update 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "uz.toshmatov.currency"
         minSdk = 21
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.1"
+        versionCode = 11
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
